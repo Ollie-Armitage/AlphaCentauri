0e50e3e6e352688c591de624da17f29f35e03025
Ollie <oliverrarmitage@gmail.com>
2021-12-27 13:48:40 +0000

    Added Test Upload React App

---
+++ frontend-test-app/src/Upload.js
@@ -0,0 +1,33 @@
+import {Component} from "react";
+import axios from 'axios';
+
+
+class Upload extends Component {
+     state = {
+         selectedFile: null
+     }
+
+     onFileChange = event => {
+         this.setState({selectedFile: event.target.files[0]});
+     }
+
+     onFileUpload = () => {
+         const formData = new FormData();
+         formData.append("uploadedFile", this.state.selectedFile, this.state.selectedFile.name);
+
+         axios.post("https://localhost:7278/Upload", formData).then(r => console.log(r))
+     }
+
+    render() {
+        return (
+            <div>
+                <input type="file" name="file" onChange={this.onFileChange} />
+                <div>
+                    <button onClick={this.onFileUpload}>Submit</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Upload
