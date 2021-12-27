cbf5926cb925764c49e62d7013002d288ec70b5e
Ollie <oliverrarmitage@gmail.com>
2021-12-27 13:49:30 +0000

    CORS

---
+++ AlphaCentauri/Controllers/UploadController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlphaCentauri.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class UploadController : ControllerBase
+{
+    public UploadController()
+    {
+        
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post()
+    {
+        var request = HttpContext.Request;
+        
+        
+        
+        return Ok();
+    }
+}
